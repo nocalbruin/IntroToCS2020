7c0d7750ac658c83f2cd5c7cc61e72667c21269c
nocalbruin <48110870+nocalbruin@users.noreply.github.com>
2020-10-02 11:52:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+# This is a comment
 print ("Hello world")
 print ("adding ways to compile and run code")
-number = int(input("enter a number "))
-// Python not running
+number = int(input("enter a number: "))
